c607360d0f0cf28bfe31c1a5160df136717df3b9
Sergio Arias <s-andres140@hotmail.com>
2025-01-05 06:02:04 -0500

    feat: :sparkles: 6.3: Anecdotes, step 1 - add functionality to vote for an anecdote with Redux store
    Implemented voting functionality for anecdotes as part of exercise 6.3: Anecdotes, step 1. Utilized Redux for state management."

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -22,8 +22,27 @@ const initialState = anecdotesAtStart.map(asObject)
 const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
+  switch(action.type) {
+    case 'VOTE_ANECDOTE': {
+      const id = action.payload.id
+      const anecdoteToChange = state.find(n => n.id === id)
+      const changedAnecdote = {
+        ...anecdoteToChange,
+        votes: anecdoteToChange.votes + 1
+      }
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : changedAnecdote) 
+    }
+    default:
+      return state
+  }
+}
 
-  return state
+export const voteAnecdote = (id) => {
+  return {
+    type: 'VOTE_ANECDOTE',
+    payload: { id }
+  }
 }
 
 export default reducer
