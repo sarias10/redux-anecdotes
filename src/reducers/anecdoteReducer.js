38b4d408a48a5981ffe68a72e28230179caf824d
Sergio Arias <s-andres140@hotmail.com>
2025-01-05 06:41:31 -0500

    feat: :sparkles: 6.4: Anecdotes, step 2 - add functionality to create new anecdotes
    Implemented functionality to allow users to add new anecdotes. This feature updates the Redux store accordingly.

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -33,6 +33,8 @@ const reducer = (state = initialState, action) => {
       return state.map(anecdote =>
         anecdote.id !== id ? anecdote : changedAnecdote) 
     }
+    case 'NEW_ANECDOTE':
+      return [...state, action.payload]
     default:
       return state
   }
@@ -42,6 +44,14 @@ export const voteAnecdote = (id) => {
   return {
     type: 'VOTE_ANECDOTE',
     payload: { id }
+  }
+}
+
+export const createAnecdote = (content) => {
+  const newNote = asObject(content)
+  return {
+    type: 'NEW_ANECDOTE',
+    payload: newNote
   }
 }
 
