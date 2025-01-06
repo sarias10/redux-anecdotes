9baee2017b50700518e087f18a872e6a28e79ed3
Sergio Arias <s-andres140@hotmail.com>
2025-01-06 06:52:40 -0500

    feat: :sparkles: 6.7: Anecdotes, step 5. create AnecdoteForm component
    Separated the creation of new anecdotes into a dedicated AnecdoteForm component.       Moved all logic for creating a new anecdote to this new component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { createAnecdote, sortAnecdotes, voteAnecdote } from './reducers/anecdoteReducer'
+import { voteAnecdote } from './reducers/anecdoteReducer'
+import AnecdoteForm from './components/AnecdoteForm'
 
 const App = () => {
   const anecdotes = useSelector(state => state)
@@ -8,14 +9,6 @@ const App = () => {
   const vote = (id) => {
     console.log('vote', id)
     dispatch(voteAnecdote(id))
-  }
-
-  const addAnecdote = (event) => {
-    event.preventDefault()
-    const content = event.target.anecdote.value
-    event.target.anecdote.value = ''
-    dispatch(createAnecdote(content))
-    dispatch(sortAnecdotes())
   }
 
   return (
@@ -33,10 +26,7 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form onSubmit={addAnecdote}>
-        <div><input name="anecdote"/></div>
-        <button type="submit">create</button>
-      </form>
+      <AnecdoteForm/>
     </div>
   )
 }
