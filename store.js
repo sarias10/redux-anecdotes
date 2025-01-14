d1763a2f5ace72e1e66b7e275e6da035f983f6ce
Sergio Arias <s-andres140@hotmail.com>
2025-01-14 05:51:25 -0500

    feat: :sparkles: integrate Redux Toolkit and enhance store creation (exercise 6.10 step 8)
    - Installed Redux Toolkit and moved Redux store creation to store.js using configureStore.
    - Refactored the filters reducer and action creators to use createSlice from Redux Toolkit.
    - Integrated Redux DevTools for improved application state debugging.

---
+++ store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import anecdoteReducer from "./src/reducers/anecdoteReducer";
+import filterReducer from "./src/reducers/filterReducer";
+
+export const store = configureStore({
+    reducer: {
+        anecdotes: anecdoteReducer,
+        filter: filterReducer
+    }
+})
