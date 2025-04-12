4cb3c51d377b3fd2b2083f187a51c647b62af8d3
Sergio Arias <s-andres140@hotmail.com>
2025-04-12 14:02:18 -0500

    refactor: :recycle: 6.17 Anecdotes and the Backend, step 4
    refactor createAnecdote for keep the asynchronouse anecdote creation in anecdoteReducer

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,19 +1,20 @@
 import { useDispatch } from "react-redux"
-import { appendAnecdote, createAnecdote, sortAnecdotes } from "../reducers/anecdoteReducer"
+import { createAnecdote, sortAnecdotes } from "../reducers/anecdoteReducer"
 import { clearNotification, createAnecdoteNotification } from "../reducers/notificationReducer"
-import anecdoteService from '../services/anecdotes'
+
 const AnecdoteForm = () => {
     const dispatch = useDispatch()
+
     const addAnecdote = async (event) => {
         event.preventDefault()
         const content = event.target.anecdote.value
         event.target.anecdote.value = ''
-        const newAnecdote = await anecdoteService.createNew(content)
-        dispatch(createAnecdote(newAnecdote))
+        dispatch(createAnecdote(content))
         dispatch(sortAnecdotes())
         dispatch(createAnecdoteNotification(content))
         dispatch(clearNotification)
       }
+
   return (
     <>
         <h2>create new</h2>
